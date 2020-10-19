8acac8921055ab096721f04ffb4292e9c3dfd04e
JeanCarlos911 <jeancarlosodi@gmail.com>
2020-10-19 09:39:01 -0500

    se ha reparado el metodo de escalonar matriz

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 
 public class Main {
+
     public static double determinante=1;
 
     public static void main(String[] args) {
@@ -39,70 +40,55 @@ public class Main {
     }
 
     public static int calcularMatrizEscalonada(double[][] matriz) {
-        int i=0, j=0, c=0, limI=0, i1, j1;
+        int c = 0; //contador de operaciones elementales
+        int n = matriz.length; //cantidad de filas
+        int m = matriz[0].length; // cantidad de columnas
 
-        while(true){//5*4 + 3 + 7*3 + 9*9  + 3*3 + 3*11 + 11*12 + 3*4 + 6  (varía dependiendo de la matriz) 3*4
-                    //5*5 + 4 + 7*4 + 10*9 + 4*3 + 6*11 + 20*12 + 4*4 + 6                                   4*4
-            //5*(n+1) + n + 7*n +  (n + 6)*9 + n*3 + (3*n-6)*11 + (9*n - m*m)*12 + n*4 + 6
-            //5n + 5 + n + 7n + 9n + 54 + 3n + 33n - 66 + 108n - 12m^2 + nm + 6
-            //-12m^2 + nm + 166n - 1
+        //itera por cada fila de la matriz
+        for(int i=0; i<n; i++) {
 
-            if(i<matriz.length && j == matriz[0].length-1 && matriz[i][j] != 0.0){
-                c+=83;
-                matriz[i][j] = 1;
-                for(int i2=i+1; i2<matriz.length; i2++)
-                    matriz[i2][j] = 0;
-                break;
-            }
-            c+=7;
-            if(j<matriz[0].length && i<matriz.length && matriz[i][j] != 0) {
-                /*
-                if(i != j){//por si hace falta hacer un cambio de filas
-                    double[] temp = matriz[i];
-                    matriz[i] = matriz[j];
-                    matriz[j] = temp;
-                }*/
-                c+=4 + 3;
-                double temp=1/matriz[i][j];
-                for(int k=j; k<matriz[0].length; k++) {
-                    c+=9;
-                    matriz[i][k] = matriz[i][k] * temp;
-                }
-                //hace cero los valores por debajo del pivote
-                c+=3;
-                for (i1=i + 1; i1 < matriz.length; i1++) {
-                    c+=11;
-                    temp = matriz[i1][j] / matriz[i][j];//toma el valor que al sumar cumple la condición
+            //itera por cada columna de la matriz
+            for(int j=i; j<m; j++) {
 
-                    for(j1=j; j1<matriz[0].length; j1++) {
-                        c+=12;
-                        matriz[i1][j1] = matriz[i1][j1] - temp * matriz[i][j1]; //sumo a toda la fila
+                //En caso de que se necesite intercambiar dos filas
+                for(int i1=i; i1<n; i1++) {
+                    if (matriz[i1][j] != 0.0) {
+                        double[] temp = matriz[i];
+                        matriz[i] = matriz[i1];
+                        matriz[i1] = temp;
+                        break;
                     }
                 }
-                c+=4;
-                limI=j+1;
-                i = limI;
-                j = limI;
-                continue;
+
+                //si el pivote es cero después de organizar las filas, busca en la siguiente columna
+                if(matriz[i][j] == 0.0)
+                    continue;
+                //sino, procede a hacer el pivote 1 y los valores inferiores del pivote a 0
+                else {
+                    //aquí hace al pivote 1
+                    double temp = matriz[i][j];
+                    for(int j1=j; j1<m; j1++) {
+                        matriz[i][j1] = matriz[i][j1] / temp;
+                    }
+
+                    //aquí hace a los valores debajo del pivote 0
+                    if(i < n-1) {
+                        for (int i1 = i+1; i1<n; i1++) {
+                            temp = matriz[i1][j];
+
+                            for(int j1=j; j1<m; j1++) {
+                                matriz[i1][j1] = matriz[i1][j1] - matriz[i][j1] * temp;
+                            }
+                        }
+                    }
+
+                    //continua el proceso con la siguiente fila
+                    break;
+                }
+
             }
 
-            //contador: aquí entra si la matriz esta no es cuadrada o no esta full de pivotes o en la ultima comparacion
-            c+=3;
-            //si no se llega a encontrar pivote en alguna columna o si es la última iteración
-            if(i >= matriz.length - 1){
-               c+=3;
-                if(j < matriz[0].length - 1){
-                    j++;
-                    c++;
-                }else break;
-                c++;
-                i = limI;
-            }else {
-                c++;
-                i++;
-            }
         }
-        c++;
 
         return c;
     }
